61b455f7b2b9d174a6b5d8f27ea0b0e00b7f6f74
Andre Dragacevac <a.dragacevac@icloud.com>
2022-10-18 03:07:28 -0400

    commit tree 744641548e1349b317ae13fb023e2ce4c3176321

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names) {
+    const messages = [];
+    
+      for  (let i=0; i< names.length; i++) {
+    
+      const message = (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      messages.push(message);
+      }
+      return messages;
+    }
+    console.log(writeCards(['Guadalupe','Ollie','Aki']));
+
+function countDown(){
+    let countDown = 10;
+    while (countDown>=0){
+    console.log(countDown--);
+    }
+}
